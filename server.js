adac198be1db422760f3ccc97f3f21deb751f866
Harsh <harshsingh94070@gmail.com>
2024-07-02 21:49:44 +0530

    Shift Mongodb to online server and use dotenv

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express=require("express");
 const db=require('./db');
 const bodyparser=require('body-parser');
 const app=express();
+
+require('dotenv').config();
+
 const studentroutes=require("./Routes/studentRoutes");
 app.use(bodyparser.json());
+
+const PORT=process.env.PORT || 3000;
+
 app.get('/',(req,res)=>{
     res.send('Welcome to Detailmania');
 })
 
 app.use('/s',studentroutes);
 
-app.listen(3000, () => {
+
+
+
+app.listen(PORT, () => {
     console.log('Listening on port 3000');
   });
 // comment first
